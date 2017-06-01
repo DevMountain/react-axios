26c36f7e3daebf6e01b28349d56be4e958da1144
James Lemire <webdev.jameslemire@gmail.com>
2017-05-31 18:32:51 -0600

    JL readme-updates - :construction:

--- src/components/Workspace/Customer/RepairLog/RepairLog.js
+++ src/components/Workspace/Customer/RepairLog/RepairLog.js
@@ -1,49 +1,12 @@
 import React, { Component } from "react";
+import ToggleEdit from '../ToggleEdit/ToggleEdit';
 import './RepairLog.css';
 
-export default class RepairLog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      log: this.props.log,
-      editting: false
-    };
-
-    this.handleChange = this.handleChange.bind( this );
-    this.toggle = this.toggle.bind( this );
-    this.save = this.save.bind( this );
-  }
-
-  handleChange( event ) {
-    this.setState({ log: event.target.value })
-  }
-
-  toggle() {
-    this.setState({ editting: !this.state.editting });
-  }
-
-  save() {
-    this.setState({ editting: !this.state.editting });
-  }
-
-  render() {
-    const { editting } = this.state;
+export default function RepairLog({ id, log }) {
     return (
       <div id="CustomerRepairLog__container">
         <h5> Repair Log </h5>
-        <textarea id="CustomerRepairLog__log" disabled={ !editting } value={ this.state.log } onChange={ this.handleChange } />
-        {
-          editting
-          ?
-            <div style={ { display: 'inline' } }>
-              <button id="CustomerRepairLog__editBtn" onClick={ this.toggle }> X </button>
-              <button id="CustomerRepairLog__saveBtn" onClick={ this.save }> Save </button>
-            </div>
-          :
-            <button id="CustomerRepairLog__editBtn" onClick={ this.toggle }> Edit </button>
-        }
+        <ToggleEdit multi id={ id } property="log" val={ log } readOnlyVal={ log }/>
       </div>
     )
-  }
-
 }
