4dc5bee01028c9ed7bf078f3539e1540772fcf9e
Aaron Bowers <Aaronjbowers@gmail.com>
2017-08-29 11:19:41 -0700

    removing customers.js

--- src/customers.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import apiURL from './api';
-
-export const getCustomerList = function() {
-  return axios.get(apiURL).then(response => {
-    return response.data;
-  })
-}
